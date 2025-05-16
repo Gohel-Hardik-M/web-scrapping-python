0cd29d3aaacd1268a8bac61214b6db8b103a72ea
GOHEL HARDIK <hardikmg21@gmail.com>
2025-05-16 13:38:56 +0530

    Add files via upload

---
+++ WEB SCRAPING (BeautifulSoup, urllib)/get_all_jpg_images.py
@@ -0,0 +1,19 @@
+from urllib import urlopen 
+from bs4 import BeautifulSoup 
+import re
+from urllib.error import HTTPError
+
+def get_all_jpg_images(url):
+    try:
+         html = urlopen(url)
+    except HTTPError as e :
+        return None
+    else :
+        try:
+            bsObj = BeautifulSoup(html)
+            images = bsObj.findAll({"img",re.compile("\.\.\/img/gifts/img.*\.jpg")})
+        
+        except AttributeError as e:
+            return None
+    return images
+
